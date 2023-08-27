c49392cff56d79eb67fde76c8585254bada98927
Vivek <vivek.sharma@mpokket.com>
2023-08-27 15:51:10 +0530

    NetworkModule added

---
+++ core/network/src/main/java/com/core/network/di/NetworkModule.kt
@@ -0,0 +1,21 @@
+package com.core.network.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://api.themoviedb.org/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+}
